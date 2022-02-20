dc359caf40960e910abf0c7c41039c978d985bd9
AlenaVasileva <Alenadvasileva@yandex.ru>
2022-02-20 12:47:38 +0300

    added connection

---
+++ CryptoRatingMobileApp/CryptoRatingMobileApp/Services/ServerConnect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.JSON
+
+namespace CryptoRatingMobileApp.Services
+{
+    internal static class ServerConnect
+    {
+
+    }
+
+    public static class Profile
+    {
+        public double absolute_change_24h;
+        public double assets_value;
+        public double relative_change_24h;
+    }
+}
